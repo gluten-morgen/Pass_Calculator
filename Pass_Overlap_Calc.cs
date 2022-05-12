faed9e0eb463496c6e11081b7a34c6f63fd46e57
gluten-morgen <86211204+gluten-morgen@users.noreply.github.com>
2022-05-12 00:19:54 -0600

    Initial commit

--- Pass_Overlap_Calc.cs
+++ Pass_Overlap_Calc.cs
@@ -3,13 +3,16 @@
 
 class Pass_Overlap_Calc
 {
-    public double L, pitch, pass, pass_width;
-    public double tolerance_min, tolerance_max;
+    public float L, nom_pitch, pass_width;
+    public float tolerance_min, tolerance_max;
+
+    public static int iteration_cntr;
+    public static float pass;
 
     public Pass_Overlap_Calc()
     {
         L = 0.0f;
-        pitch = 0.0f;
+        nom_pitch = 0.0f;
         pass = 0.0f;
         pass_width = 0.0f;
 
@@ -21,70 +24,98 @@ class Pass_Overlap_Calc
     {
         Console.Write("L :");
         L = (float)Convert.ToDouble(Console.ReadLine());
-        L = Math.Round(L, 2);
+        L = (float)Math.Round(L, 2);
 
         Console.Write("pitch: ");
-        pitch = (float)Convert.ToDouble(Console.ReadLine());
-        pitch = Math.Round(pitch, 2);
+        nom_pitch = (float)Convert.ToDouble(Console.ReadLine());
+        nom_pitch = (float)Math.Round(nom_pitch, 2);
 
         Console.Write("pass width: ");
         pass_width = (float)Convert.ToDouble(Console.ReadLine());
+        pass_width = (float)Math.Round(pass_width, 2);
 
         Console.Write("Tolerance min: ");
         tolerance_min = (float)Convert.ToDouble(Console.ReadLine());
-        tolerance_min = Math.Round(tolerance_min, 2);
+        tolerance_min = (float)Math.Round(tolerance_min, 2);
 
         Console.Write("Tolerance max: ");
         tolerance_max = (float)Convert.ToDouble(Console.ReadLine());
-        tolerance_max = Math.Round(tolerance_max, 2);
+        tolerance_max = (float)Math.Round(tolerance_max, 2);
     }
 
-    public void Calculate(double pitch)
+    public void CalculatePasses()
     {
-        double error_pitch, error_length, pitch_1, final;
+        float error_pitch, error_length, pitch_temp, pitch_final;
 
-        double L_calc = L - pass_width;
+        float L_calc = L - pass_width;
 
-        pass = L_calc / pitch;
+        pass = L_calc / nom_pitch;
         pass += 1;
 
-        pass = Math.Round(pass);
-        pitch_1 = L_calc / pass;
+        pass = (float)Math.Round(pass);
+        pitch_temp = L_calc / pass;
 
-        final = Iterations(pitch_1);
-        final = Math.Round(final, 2);
+        iteration_cntr = 0;
+        pitch_final = Iterations(pitch_temp);
+        pitch_final = (float)Math.Round(pitch_final, 2);
 
-        Console.WriteLine("{0} passes at {1}mm pitch.", pass, final);
+        error_length = Math.Abs(((pitch_final * pass) + pass_width) - L);
+        error_length = (float)Math.Round(error_length, 2);
+
+        /*if (pitch_final == -0.1f) pass++;
+        else if (pitch_final == 0.1f) pass--;*/
+
+        Console.WriteLine("{0} passes at {1}mm pitch, with {2}mm error in length.", pass, pitch_final, error_length);
 
         
     }
 
-    public double Iterations(double pitch_1)
+    public float Iterations(float pitch_calc)
     {
-        double error_pitch = (pitch_1 - pitch) / pitch;
-        double error_length = Math.Abs(((pitch_1 * pass) + pass_width) - L);
+        float error_pitch, error_length;
 
-        error_pitch = Math.Round(error_pitch, 2);
-        error_length = Math.Round(error_length, 2);
+        error_pitch = (pitch_calc - nom_pitch) / nom_pitch;
+        //error_length = ((pitch_calc * pass) + pass_width) - L;
+
+        error_pitch = (float)Math.Round(error_pitch, 2);
+        //error_length = (float)Math.Round(error_length, 2);
 
         if (In_tolerance(error_pitch))
         {
-            return pitch_1;
+            return pitch_calc;
         }
         else
         {
-            if (error_pitch < tolerance_min)
+            if (error_pitch <= tolerance_min)
                 error_pitch = tolerance_min - error_pitch;
-            else if (error_pitch > tolerance_max)
+            else if (error_pitch >= tolerance_max)
                 error_pitch = error_pitch - tolerance_max;
 
-            pitch_1 = pitch_1 * (1 + error_pitch);
-            pitch_1 = Math.Round(pitch_1, 2);
+            pitch_calc = pitch_calc * (1 + error_pitch);
+            pitch_calc = (float)Math.Round(pitch_calc, 2);
 
-            error_pitch = (pitch_1 - pitch) / pitch;
-            error_length = Math.Abs(((pitch_1 * pass) + pass_width) - L);
+            error_length = ((pitch_calc * pass) + pass_width) - L;
+            error_length = (float)Math.Round(error_length, 2);
 
-            return Iterations(pitch_1);
+            iteration_cntr++;
+
+            if (iteration_cntr % 2 == 0)
+            {
+                if (iteration_cntr >= 6) return 0.0f;
+            }
+
+            if (error_length >= 1)
+            {
+                pass -= 1;
+            }
+            else if (error_length <= -1)
+            {
+                pass += 1;
+            }
+
+            if (In_tolerance(error_pitch) && Math.Abs(error_length) < 1) return pitch_calc;
+
+            return Iterations((L - pass_width) / pass);
         }
     }
 
@@ -100,6 +131,6 @@ class Pass_Overlap_Calc
         Pass_Overlap_Calc calc = new();
 
         calc.Input();
-        calc.Calculate(3.2);
+        calc.CalculatePasses();
     }
 }
